84e92f2c604b0e90f753a169bbbd2950b26b5707
Kuma-0x0 <yuta.kumagai.09@gmail.com>
2023-03-06 23:30:04 +0900

    [add]EnumrableUtility追加。

---
+++ Utility.Test/Process/EnumerableUtilityTest.cs
@@ -0,0 +1,63 @@
+﻿using Utility.Process;
+
+namespace Utility.Test.Process
+{
+    [TestClass]
+    public class EnumerableUtilityTest
+    {
+        [TestMethod]
+        public void AnyOrNotNull_01_Null_期待値_FALSE()
+        {
+            IEnumerable<int>? data = null;
+            var actual = EnumerableUtility.AnyOrNotNull(data);
+            Assert.IsFalse(actual);
+
+            actual = data.AnyOrNotNull();
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void AnyOrNotNull_02_空配列_期待値_FALSE()
+        {
+            var data = Array.Empty<int>();
+            var actual = EnumerableUtility.AnyOrNotNull(data);
+            Assert.IsFalse(actual);
+
+            actual = data.AnyOrNotNull();
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void AnyOrNotNull_03_空リスト_期待値_FALSE()
+        {
+            var data = new List<int>();
+            var actual = EnumerableUtility.AnyOrNotNull(data);
+            Assert.IsFalse(actual);
+
+            actual = data.AnyOrNotNull();
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void AnyOrNotNull_04_要素あり配列_期待値_TRUE()
+        {
+            var data = new int[] { 0, };
+            var actual = EnumerableUtility.AnyOrNotNull(data);
+            Assert.IsTrue(actual);
+
+            actual = data.AnyOrNotNull();
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void AnyOrNotNull_05_要素ありリスト_期待値_TRUE()
+        {
+            var data = new List<int>() { 0, };
+            var actual = EnumerableUtility.AnyOrNotNull(data);
+            Assert.IsTrue(actual);
+
+            actual = data.AnyOrNotNull();
+            Assert.IsTrue(actual);
+        }
+    }
+}
---
+++ Utility/Process/EnumerableUtility.cs
@@ -0,0 +1,13 @@
+﻿namespace Utility.Process
+{
+    public static class EnumerableUtility
+    {
+        /// <summary>
+        /// 要素が含まれているか。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static bool AnyOrNotNull<T>(this IEnumerable<T>? target) => target?.Any() ?? false;
+    }
+}

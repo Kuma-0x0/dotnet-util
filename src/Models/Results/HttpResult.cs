db02b5f656e8acb6361188959f8e475562e852a2
Kuma-0x0 <yuta.kumagai.09@gmail.com>
2024-05-26 22:30:32 +0900

    [add]Result モデル追加

---
+++ src/Models/Results/HttpResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Models.Results;
+
+public static class HttpResultFactory
+{
+    public static HttpResult<object?> CreateSuccessResult(HttpStatusCode statusCode) => new(statusCode, null, null, null);
+
+    public static HttpResult<T> CreateSuccessResult<T>(HttpStatusCode statusCode, T value) => new(statusCode, value, null, null);
+
+    public static HttpResult<T> CreateWarningResult<T>(HttpStatusCode statusCode, string warning) => new(statusCode, default, warning, null);
+
+    public static HttpResult<T> CreateWarningResult<T>(HttpStatusCode statusCode, T value, string warning) => new(statusCode, value, warning, null);
+
+    public static HttpResult<object?> CreateErrorResult(HttpStatusCode statusCode, string error) => new(statusCode, null, null, error);
+}
+
+public record HttpResult<T>(
+    HttpStatusCode StatusCode,
+    T? Value,
+    string? Warning,
+    string? Error
+) : Result<T>(
+    IsSuccess: 200 <= (int)StatusCode && (int)StatusCode <= 299,
+    Value,
+    Warning,
+    Error
+);
